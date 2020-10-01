d0a625118bed59017851ea47f235dab4b9fa0d43
tilak-codes <62074842+tilak-codes@users.noreply.github.com>
2020-10-01 19:22:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import text_to_speech as speech
 import PyPDF2
 print("hello")
-speech.speak("hello","en")
-print(2+7)
+for i in range (1,10): 
+speech.speak("hello Tilak","en")
+print("hello world")
+print("Welcome to python")
+print("this program helps you to understand the speech facilites given by python ")
